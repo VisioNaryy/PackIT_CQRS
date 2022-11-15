202f2210e8b9ca59321bcbf0559901b700a27799
Daniil Okonskiy <dofeternity@gmail.com>
2022-11-15 13:53:18 +0600

    Added Entity, Value Objects and Exceptions to Domain layer

---
+++ PackIT.Domain/Entities/PackingList.cs
@@ -0,0 +1,32 @@
+﻿using PackIT.Domain.Exceptions;
+using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Entities;
+
+public class PackingList
+{
+    public Guid Id { get; private set; }
+
+    private PackingListName _name;
+    private Localization _localization;
+    private readonly LinkedList<PackingItem> _items;
+
+    internal PackingList(Guid id, string name, Localization localization, LinkedList<PackingItem> items)
+    {
+        Id = id;
+        _name = name;
+        _localization = localization;
+        _items = items;
+    }
+
+    public void AddItem(PackingItem item)
+    {
+        var alreadyExists = _items.Any(x => x.Name == item.Name);
+
+        //TODO: can be reconstructed to generate some response to client with exception message or putted to custom validator.
+        if (alreadyExists)
+            throw new PackingItemAlreadyExistsException(_name, item.Name);
+
+        _items.AddLast(item);
+    }
+}
---
+++ PackIT.Domain/ValueObjects/PackingItem.cs
@@ -0,0 +1,20 @@
+﻿using PackIT.Domain.Exceptions;
+
+namespace PackIT.Domain.ValueObjects;
+
+public class PackingItem
+{
+    public string Name { get; set; }
+    public uint Quantity { get; set; }
+    public bool IsPacked { get; set; }
+
+    public PackingItem(string name, uint quantity, bool isPacked)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new EmptyPackingItemNameException();
+        
+        Name = name;
+        Quantity = quantity;
+        IsPacked = isPacked;
+    }
+}
