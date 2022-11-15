fe6b92045b6ee1eec3d8aa6a9b958e642f742b10
Daniil Okonskiy <dofeternity@gmail.com>
2022-11-15 16:31:40 +0600

    Created IPackingListFactory

---
+++ PackIT.Domain/Factories/PackingListFactory.cs
@@ -0,0 +1,18 @@
+﻿using PackIT.Domain.Consts;
+using PackIT.Domain.Entities;
+using PackIT.Domain.ValueObjects;
+
+namespace PackIT.Domain.Factories;
+
+public class PackingListFactory : IPackingListFactory
+{
+    public PackingList Create(PackingListId id, PackingListName name, Localization localization)
+    {
+        throw new NotImplementedException();
+    }
+
+    public PackingList CreateWithDefaultItems(PackingListId id, PackingListName name, TravelDays days, Temperature temperature, Gender gender)
+    {
+        throw new NotImplementedException();
+    }
+}
