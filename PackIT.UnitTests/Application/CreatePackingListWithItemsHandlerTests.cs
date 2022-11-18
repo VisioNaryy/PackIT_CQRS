f83e74ec1c00353ae8689735e9ba36f5ced89da7
Daniil Okonskiy <dofeternity@gmail.com>
2022-11-18 21:12:47 +0600

    Added Unit Testing of Domain and Application layers

---
+++ PackIT.UnitTests/Application/CreatePackingListWithItemsHandlerTests.cs
@@ -0,0 +1,94 @@
+﻿using NSubstitute;
+using PackIT.Application.Commands;
+using PackIT.Application.Commands.Handlers;
+using PackIT.Application.DTO.External;
+using PackIT.Application.Exceptions;
+using PackIT.Application.Services;
+using PackIT.Domain.Consts;
+using PackIT.Domain.Entities;
+using PackIT.Domain.Factories;
+using PackIT.Domain.Repository;
+using PackIT.Domain.ValueObjects;
+using PackIT.Shared.Abstractions.Commands;
+using Shouldly;
+
+namespace PackIT.UnitTests.Application;
+
+public class CreatePackingListWithItemsHandlerTests
+{
+    Task Act(CreatePackingListWithItems command)
+        => _commandHandler.HandleAsync(command);
+
+    [Fact]
+    public async Task HandleAsync_Throws_PackingListAlreadyExistsException_When_List_With_The_Same_Name_Already_Exists()
+    {
+        var command = CreateCommand();
+
+        _readService.ExistsByNameAsync(command.Name).Returns(true);
+
+        var exception = await Record.ExceptionAsync(() => Act(command));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<PackingListAlreadyExistsException>();
+    }
+
+    [Fact]
+    public async Task
+        HandleAsync_Throws_MissingLocalizationWeatherException_When_Weather_Is_Not_Returned_From_Weather_Service()
+    {
+        var command = CreateCommand();
+
+        _readService.ExistsByNameAsync(command.Name).Returns(false);
+        _weatherService.GetWeatherAsync(Arg.Any<Localization>()).Returns(default(WeatherDto));
+
+        var exception = await Record.ExceptionAsync(() => Act(command));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<MissingLocalizationWeatherException>();
+    }
+
+    [Fact]
+    public async Task HandleAsync_Calls_Repository_On_Success()
+    {
+        var command = CreateCommand();
+
+        _readService.ExistsByNameAsync(command.Name).Returns(false);
+        _weatherService.GetWeatherAsync(Arg.Any<Localization>()).Returns(new WeatherDto(12));
+
+        var (id, name, days, gender, _) = command;
+
+        _factory.CreateWithDefaultItems(id, name, days, Arg.Any<Temperature>(), gender, Arg.Any<Localization>())
+            .Returns(default(PackingList));
+
+        var exception = await Record.ExceptionAsync(() => Act(command));
+
+        exception.ShouldBeNull();
+        await _repository.Received(1).AddAsync(Arg.Any<PackingList>());
+    }
+
+
+    #region ARRANGE
+
+    private readonly ICommandHandler<CreatePackingListWithItems> _commandHandler;
+    private readonly IPackingListRepository _repository;
+    private readonly IWeatherService _weatherService;
+    private readonly IPackingListReadService _readService;
+    private readonly IPackingListFactory _factory;
+
+
+    public CreatePackingListWithItemsHandlerTests()
+    {
+        _repository = Substitute.For<IPackingListRepository>();
+        _weatherService = Substitute.For<IWeatherService>();
+        _readService = Substitute.For<IPackingListReadService>();
+        _factory = Substitute.For<IPackingListFactory>();
+
+        _commandHandler = new CreatePackingListWithItemsHandler(_repository, _factory, _readService, _weatherService);
+    }
+
+    private CreatePackingListWithItems CreateCommand()
+        => new CreatePackingListWithItems(Guid.NewGuid(), "MyList", 10, Gender.Female,
+            new LocalizationWriteModel("Warsaw", "Poland"));
+
+    #endregion
+}
---
+++ PackIT.UnitTests/Domain/PackingListTests.cs
@@ -0,0 +1,60 @@
+﻿using PackIT.Domain.Entities;
+using PackIT.Domain.Events;
+using PackIT.Domain.Exceptions;
+using PackIT.Domain.Factories;
+using PackIT.Domain.Policies;
+using PackIT.Domain.ValueObjects;
+using Shouldly;
+
+namespace PackIT.UnitTests.Domain;
+
+public class PackingListTests
+{
+    [Fact]
+    public void AddItem_Throws_PackingItemAlreadyExistsException_When_There_Is_Already_Item_With_The_Same_Name()
+    {
+        var packingList = GetPackingList();
+        packingList.AddItem(new PackingItem("Item1", 1));
+
+        var exception = Record.Exception(() => packingList.AddItem(new PackingItem("Item1", 1)));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<PackingItemAlreadyExistsException>();
+    }
+
+    [Fact]
+    public void AddItem_Adds_PackingItemAdded_Domain_Event_On_Success()
+    {
+        var packingList = GetPackingList();
+
+        var exception = Record.Exception(() => packingList.AddItem(new PackingItem("Item1", 1)));
+
+        var @event = packingList.Events.FirstOrDefault() as PackingItemAdded;
+        
+        exception.ShouldBeNull();
+        
+        // Can be used any of these assertions for checking the added event
+        packingList.Events.Count().ShouldBe(1);
+        @event.ShouldNotBeNull();
+        @event.ShouldBeOfType<PackingItemAdded>();
+    }
+
+    #region ARRANGE
+
+    private readonly IPackingListFactory _factory;
+
+    public PackingListTests()
+    {
+        _factory = new PackingListFactory(Enumerable.Empty<IPackingItemsPolicy>());
+    }
+
+    private PackingList GetPackingList()
+    {
+        var packingList = _factory.Create(Guid.NewGuid(), "TestList", Localization.Create("Warsaw,Poland"));
+        packingList.ClearEvents();
+
+        return packingList;
+    }
+
+    #endregion
+}
